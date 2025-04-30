b10241de4d31dc8fd29b82a88958c4b3bbdf06c6
Firas Jelassi <49401280+FirasSharp@users.noreply.github.com>
2025-04-30 23:41:02 +0200

    WIP added download link extraction and url validation

--- pkg/ff.go
+++ pkg/ff.go
@@ -3,10 +3,15 @@ package pkg
 
 import (
 	"errors"
-	"time"
+	"strings"
+
+	"io"
+	"net/http"
+	"net/url"
 
 	"github.com/vbauerster/mpb"
 	_ "github.com/vbauerster/mpb/decor"
+	"mvdan.cc/xurls/v2"
 )
 
 type FF struct {
@@ -21,19 +26,59 @@ func NewFF(url string, progress *mpb.Progress, spinner *mpb.Bar) (*FF, error) {
 	ff := new(FF)
 	ff.url = url
 	ff.progress = progress
-	return ff, ff.parseAndValidate(spinner)
+	return ff, ff.checkURLAndExtractDownload(spinner)
 }
 
-func (ff *FF) parseAndValidate(spinner *mpb.Bar) error {
+func (ff *FF) checkURLAndExtractDownload(spinner *mpb.Bar) error {
 	defer spinner.Increment()
-	if ff.url == "http://invalid" {
+	if !ff.validateUrl() {
 		return errors.New("Invalid Url!")
 	}
-	time.Sleep(3 * time.Second)
+	downloadUrl, err := ff.extractDownloadLink()
+	if err != nil {
+		return err
+	}
+	ff.downloadUrl = downloadUrl
+	ff.valid = true
 	return nil
 }
 
 func (ff *FF) Download() error {
 	//bar := ff.progress.Add()
 	return nil
+}
+
+func (ff *FF) validateUrl() bool {
+	link, err := url.ParseRequestURI(ff.url)
+	if err != nil {
+		return false
+	}
+	ffdomain, _ := url.ParseRequestURI("https://fuckingfast.co/")
+	return (link.Host == ffdomain.Host) && (link.Scheme == ffdomain.Scheme)
+}
+
+func (ff *FF) extractDownloadLink() (string, error) {
+	resp, err := http.Get(ff.url)
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close()
+
+	bytes, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		return "", err
+	}
+
+	rxRelaxed := xurls.Relaxed()
+	urls := rxRelaxed.FindAllString(string(bytes), -1)
+
+	for _, url := range urls {
+		if strings.HasPrefix(url, "https://fuckingfast.co/dl/") {
+			return url, nil
+		}
+	}
+
+	return "", errors.New("Download url was not found!")
 }
